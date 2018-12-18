6624ba171dbdd66378867de93d03f1ef93a2ebaf
Arthur Egide <arthuregide@gmail.com>
2018-12-17 22:25:04 -0200

    Primeiros arquivos

---
+++ buttons.js
@@ -0,0 +1,11 @@
+<script src="jquery-3.3.1.min.js"></script>
+
+document.body.style.border = "5px solid purple";
+
+var txt3 = document.createElement("p");  // Create with DOM
+
+var para = document.createElement("span");
+var node = document.createTextNode("olaaa");
+para.appendChild(node);
+var element = document.getElementById("random5c1837e7161601_group");
+element.appendChild(para);
