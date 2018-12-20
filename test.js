953f1fa599575e15aa84a92274eae8acd103a557
Arthur Egide <arthuregide@gmail.com>
2018-12-20 00:48:02 -0200

    update

--- test.js
+++ test.js
@@ -1,12 +1,34 @@
 var play = document.querySelector('#play');
 var pause = document.querySelector('#pause');
 var video = document.querySelector('video');
+var cont = document.querySelector('#contador');
+
+
+video.onended = function() {
+  alert(video.duration);
+};
+
+video.ontimeupdate = function(){
+  cont.innerHTML = video.currentTime;
+}
+
+/*while(video.currentTime <= video.duration){
+  cont.innerHTML = video.currentTime;
+}
+
+
+ video.onplay = function(){
+  alert("Tocando");
+
+  
+
+}*/
 
 play.addEventListener('click', function(){
   video.play();
-
+  
 });
-
+video.ended
 pause.addEventListener('click', function(){
   video.pause();
 
@@ -20,6 +42,8 @@ recuar.addEventListener('click', function(){
 avancar.addEventListener('click', function(){
   //alert(video.currentTime);
   video.currentTime += 2;
+  
+
 });
 
 telaCheia.addEventListener('click', function(){
@@ -27,3 +51,11 @@ telaCheia.addEventListener('click', function(){
   video.webkitEnterFullScreen();
   video.webkitEnterFullscreen();
 });
+
+function fimVideo(){
+  if(video.ended == true){
+    return true;
+  }else{
+    return false;
+  }
+}
